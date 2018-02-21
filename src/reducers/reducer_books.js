28471213834e110ed4dc8b7fa7d51ffb4d70039e
Sraddha Kar <sraddha.kar17@gmail.com>
2018-02-21 18:53:15 +0530

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [            //returning a list of objects
+        { title : 'Javascript: The Good Part', pages : 101 },
+        { title : 'Harry Potter', pages : 233 },
+        { title : 'The Dark Tower', pages : 45 },
+        { title : 'Eloquent Ruby', pages : 456 }
+    ]
+}
